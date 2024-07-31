022931e821e222253a29c8098694b9e0d1fed798
2paperstar <lee@paperst.ar>
2024-08-01 00:50:16 +0900

    feat: move linear motor to guessed position

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,7 @@
 #include "vision.h"
 
 int main() {
-  Arm arm;
-
+  // Arm a;
   auto lm = LinearMotor(0);
   lm.guessLimits();
   Vision vision;
@@ -14,11 +13,35 @@ int main() {
   Timer timer;
 
   cv::Vec3d ballPosition;
+  std::vector<cv::Vec3d> ballHistory;
+  int missCount = 0;
+  bool success = false;
 
   do {
     if (vision.track(ballPosition)) {
-      lm.setPosition(lm.map(ballPosition[1], Y_TABLE_SIZE, 0), false);
+      ballHistory.push_back(ballPosition);
+      if (ballPosition[0] > X_TABLE_SIZE / 2 && !success) {
+        auto first = ballHistory[static_cast<int>(ballHistory.size()) / 3];
+        auto &last = ballPosition;
+        const auto dx = last[0] - first[0];
+        const auto dy = last[1] - first[1];
+
+        constexpr auto targetX = X_TABLE_SIZE - 0.35;
+        const auto targetY = last[1] + dy / dx * (targetX - last[0]);
+
+        lm.setPosition(lm.map(targetY, Y_TABLE_SIZE, 0), false);
+        success = true;
+      }
+    } else if (!ballHistory.empty()) {
+      missCount++;
+      if (missCount > 10) {
+        ballHistory.clear();
+        success = false;
+        missCount = 0;
+        lm.setPosition(lm.map(0.5, 0, 1), false);
+      }
     }
+    vision.setMachinePosition(lm.getMappedPosition(Y_TABLE_SIZE, 0));
     vision.render(timer.getFps());
   } while (!vision.stopped());
 }
