63c85451335d34aafea82649fceaa1af54c6e26c
2paperstar <lee@paperst.ar>
2024-08-08 00:06:08 +0900

    fix: hit x

--- src/predictor.cpp
+++ src/predictor.cpp
@@ -8,7 +8,7 @@
 #define TARGET_X (X_TABLE_SIZE - 0.1)
 #define FINAL_PREDICTION_Y (X_TABLE_SIZE - 0.6)
 #define FINAL_PREDICTION_Z (X_TABLE_SIZE - 0.6)
-#define HIT_X (X_TABLE_SIZE / 2)
+#define HIT_X (TARGET_X - 0.4)
 
 Predictor::Predictor() { reset(); }
 
