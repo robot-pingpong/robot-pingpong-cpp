a04e338e4eaaacffccbd319cd2c70bfd32a2e359
2paperstar <lee@paperst.ar>
2024-08-12 01:04:38 +0900

    fix: change angle constant

--- src/control/arm.cpp
+++ src/control/arm.cpp
@@ -23,11 +23,11 @@ void Arm::init() {
   shoulder.setPositionPGain(400);
 
   // wrist.setGoalVelocity(1000);
-  wrist.setProfileVelocity(1800);
-  wrist.setProfileAcceleration(450);
+  pitchWrist.setProfileVelocity(1800);
+  pitchWrist.setProfileAcceleration(450);
 
   std::thread([&] {
-    resetByZ(20);
+    resetByZ(50);
     resetted = false;
     std::this_thread::sleep_for(std::chrono::seconds(1));
     resetByZ(400);
@@ -42,7 +42,7 @@ bool Arm::inverseKinematics(const double x, const double y, const double z,
   const auto xPrime = std::sqrt(x * x + y * y);
   const auto clampedZ = std::clamp(z, 20.0, 390.0);
   constexpr auto l1 = 188.172;
-  constexpr auto l2 = 223.334;
+  constexpr auto l2 = 231.018;
   constexpr auto l3 = 30;
   const auto xn = clampedZ - l3 * std::cos(pi);
   const auto yn = xPrime - l3 * std::sin(pi);
@@ -54,10 +54,10 @@ bool Arm::inverseKinematics(const double x, const double y, const double z,
   const auto k2 = l2 * sinTheta2;
   theta1 = std::atan2(k2, k1) - std::atan2(yn, xn);
   theta3 = pi + theta1 - theta2;
-
+  yawTheta = 180 + yawTheta / M_PI * 180;
   theta1 = 180 + theta1 / M_PI * 180 + 25.0;
-  theta2 = theta2 / M_PI * 180 + 180 - (90 - 25.0) - 16.7;
-  theta3 = 90 + theta3 / M_PI * 180 - 16.7;
+  theta2 = theta2 / M_PI * 180 + 180 - (90 - 25.0) - 10.8;
+  theta3 = 180 + 10.8 + theta3 / M_PI * 180;
 
   if (std::isnan(yawTheta) || std::isnan(theta1) || std::isnan(theta2) ||
       std::isnan(theta3)) {
